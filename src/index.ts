07ace3bca14f5d217bfd42d90ffe6e5809ef7cbc
Myrotvorets <support@myrotvorets.center>
2020-07-04 19:10:21 +0300

    Export badGatewayFromError()

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ErrorResponse } from './middleware/types';
 export { notFoundMiddleware } from './middleware/notfound';
 export { errorMiddleware } from './middleware/error';
+export { badGatewayFromError } from './utils';
