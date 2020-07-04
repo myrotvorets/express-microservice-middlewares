7be1e560e55002df829bb8bc49c913bd97c9dc5f
Myrotvorets <support@myrotvorets.center>
2020-07-04 09:45:49 +0300

    badGatewayFromError()

---
+++ src/test/utils.test.ts
@@ -0,0 +1,15 @@
+import { badGatewayFromError } from '../utils';
+
+describe('badGatewayFromError', (): void => {
+    it('should return the expected response', (): void => {
+        const expected = {
+            success: false,
+            status: 502,
+            code: 'BAD_GATEWAY',
+            message: 'Error message',
+        };
+
+        const error = new Error(expected.message);
+        expect(badGatewayFromError(error)).toEqual(expected);
+    });
+});
---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+import { ErrorResponse } from '../middleware/types';
+
+export function badGatewayFromError(err: Error): ErrorResponse {
+    return {
+        success: false,
+        status: 502,
+        code: 'BAD_GATEWAY',
+        message: err.message,
+    };
+}
