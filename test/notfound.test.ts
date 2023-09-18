856ad37a98b4abbe2aa962a7301142af38bfe330
Myrotvorets <support@myrotvorets.center>
2023-09-18 11:04:01 +0300

    Add tests

--- test/notfound.test.ts
+++ test/notfound.test.ts
@@ -1,9 +1,13 @@
 import request from 'supertest';
-import express from 'express';
+import express, { type Application } from 'express';
 import { errorMiddleware, notFoundMiddleware } from '../lib';
 
-function buildServer(): express.Application {
+function buildServer(): Application {
     const server = express();
+    server.get('/test', (_req, res, next) => {
+        res.json({ success: true });
+        next();
+    });
     server.use(notFoundMiddleware);
     server.use(errorMiddleware);
     return server;
@@ -16,5 +20,10 @@ describe('notFoundMiddleware', (): void => {
             .get('/')
             .expect(404)
             .expect({ success: false, status: 404, code: 'NOT_FOUND', message: 'Not found' });
+    });
+
+    it('should honor request.route', (): Promise<unknown> => {
+        const server = buildServer();
+        return request(server).get('/test').expect(200).expect({ success: true });
     });
 });
