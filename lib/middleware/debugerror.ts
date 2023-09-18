3270b459b5a97dcc460c9aee91a17b38605a0093
Myrotvorets <support@myrotvorets.center>
2023-09-18 10:54:27 +0300

    More verbose output from debugErrorMiddleware

--- lib/middleware/debugerror.ts
+++ lib/middleware/debugerror.ts
@@ -1,6 +1,6 @@
 import type { NextFunction, Request, Response } from 'express';
 
-export function debugErrorMiddleware(err: unknown, req: Request, res: Response, next: NextFunction): void {
-    console.warn(err);
+export function debugErrorMiddleware(err: unknown, req: Request, _res: Response, next: NextFunction): void {
+    console.warn(req.ip, req.method, req.url, err);
     return next(err);
 }
